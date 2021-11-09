4900fc48cec1b8623c696d83d005c35ca22715eb
yuxwang <yuxuan.wang@epfl.ch>
2021-11-09 15:00:43 +0100

    test demo

---
+++ loopdemo.cpp
@@ -0,0 +1,21 @@
+﻿#include <malloc.h>
+#include <stdio.h>
+
+int main() {
+    char* buf = (char *)malloc(1024 * 1024 * 1024);
+    //char* buf2 = (char *)malloc(1024 * 1024 * 1024);
+    if (buf == 0) {
+        printf("Malloc failed!\n");
+        return 0;
+    }
+    int i, j, k;
+    for (i = 0; i < 1024; i++)
+        for (j = 0; j < 1024; j++)
+            for (k = 0; k < 1024; k++)
+                buf[i * 1024 * 1024 + j * 1024 + k] = i + j + k;
+ 
+    free(buf);
+
+    while (1);
+    return 0;
+}
